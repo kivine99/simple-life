2d678942db892425e36cc9fcbd50ecb9413ded54
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-12 13:35:00 -0500

    Create SimController class

---
+++ sim_controller.py
@@ -0,0 +1,8 @@
+from environment import Environment
+
+class SimController:
+    def __init__(self, environment):
+        self.__environment = environment
+
+    def update_state(self):
+        pass
