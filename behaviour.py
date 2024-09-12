2ab0342b95feab92f8f4a04fda765219d9925464
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-12 16:59:47 -0500

    Remove unused import

--- behaviour.py
+++ behaviour.py
@@ -1,13 +1,9 @@
 from abc import ABC, abstractmethod
-from animal import Animal
 
 class Behaviour(ABC):
     @abstractmethod
-    def execute(self, animal: Animal):
+    def execute(self):
         """
         Execute the behavior for the given animal.
-
-        Args:
-            animal (Animal): The animal performing the behavior.
         """
         pass
