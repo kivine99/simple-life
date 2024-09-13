f0b7403721e68b95d278584a58b0a6ceebd42a7a
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-13 14:34:36 -0500

    Support steering behaviour

--- animal.py
+++ animal.py
@@ -2,142 +2,89 @@ import math
 from abc import ABC, abstractmethod
 from memory import Memory
 from genome import Genome
+from pygame.math import Vector2
 
 class Animal(ABC):
 
     def __init__(self, 
-    position: tuple, 
-    direction: float, 
+    position: Vector2, 
+    velocity: Vector2,
+    mass: float,
+    max_force: float,
+    max_speed: float,
+    orientation: float,
     vision_range: float, 
     fov: float, 
-    max_speed: float, 
-    curr_speed: float, 
     memory: Memory, 
     genome: Genome, 
     radius: float, 
     color: tuple):
-        """
-        Args:
-            position (tuple): The initial (x, y) position of the animal.
-            direction (float): The initial direction in radians, normalized between 0 and 2π.
-            vision_range (float): The vision range of the animal.
-            fov (float): The field of view of the animal in radians.
-            max_speed (float): The maximum speed the animal.
-            curr_speed (float): The current speed of the animal.
-            memory (Memory): Memory of the animal.
-            genome (Genome): Genetic information about the animal.
-            radius (float): The radius of the animal.
-            color (tuple): The RGB color of the animal.
-        """
-        self.__position = position
-        self.__direction = direction % (2*math.pi)
-        self.__vision_range = vision_range
-        self.__fov = fov
-        self.__max_speed = max_speed
-        self.__curr_speed = curr_speed
-        self.__memory = memory
-        self.__genome = genome
-        self.__radius = radius
-        self.__color = color
+        self._position = position
+        self._velocity = velocity
+        self._mass = mass
+        self._max_force = max_force
+        self._max_speed = max_speed
+        self._orientation = orientation
+        self._vision_range = vision_range
+        self._fov = fov
+        self._memory = memory
+        self._genome = genome
+        self._radius = radius
+        self._color = color
 
-    def get_position(self) -> tuple:
-        """
-        Returns:
-            tuple: The (x, y) position of the animal.
-        """
-        return self.__position
+    def get_position(self) -> Vector2:
+        return self._position
 
-    def get_direction(self) -> float:
-        """
-        Returns:
-            float: The direction of the animal in radians.
-        """
-        return self.__direction
+    def get_orientation(self) -> float:
+        return self._orientation
 
     def get_vision_range(self) -> float:
-        """
-        Returns:
-            float: The vision range of the animal.
-        """
-        return self.__vision_range
+        return self._vision_range
 
     def get_fov(self) -> float:
-        """
-        Returns:
-            float: The field of view of the animal in radians.
-        """
-        return self.__fov
+        return self._fov
 
     def get_max_speed(self) -> float:
-        """
-        Returns:
-            float: The max speed of the animal.
-        """
-        return self.__max_speed
+        return self._max_speed
 
-    def get_curr_speed(self) -> float:
-        """
-        Returns:
-            float: The current speed of the animal.
-        """
-        return self.__curr_speed
+    def get_velocity(self) -> Vector2:
+        return self._velocity
 
-    def get_memory(self) -> Memory:
-        pass
+    # def get_memory(self) -> Memory:
+    #     pass
 
-    def get_genome(self) -> Genome:
-        pass
+    # def get_genome(self) -> Genome:
+    #     pass
 
     def get_radius(self) -> float:
-        """
-        Returns:
-            float: The radius of the animal.
-        """
         return self.__radius
 
     def get_color(self) -> tuple:
-        """
-        Returns:
-            tuple: (R, G, B) of the animal.
-        """
         return self.__color
 
-    def calculate_next_position(self) -> tuple:
-        """
-        Returns the animal's next position based on its current speed and direction. Does not modify the position.
+    def set_orientation(self, orientation: float) -> None:
+        self.__orientation = orientation%(2*math.pi)
 
-        Returns:
-            tuple: The new x, y position.
-        """
-        x = self.__position[0] + math.cos(self.__direction) * self.__curr_speed
-        y = self.__position[1] + math.sin(self.__direction) * self.__curr_speed
-        return (x, y)
+    def apply_force(self, force: Vector2) -> None:
+        if force.length() > self._max_force:
+            force = force.normalize() * self._max_force
 
-    def set_direction(self, direction: float) -> None:
-        """
-        Sets the direction of the animal. Normalized between 0 and 2π.
-        
-        Args:
-            direction (float): The new direction in radians.
-        """
-        self.__direction = direction%(2*math.pi)
+        acceleration = force / self._mass
 
-    def set_curr_speed(self, new_speed: float) -> None:
-        """
-        Sets the current speed of the animal, ensuring it's between 0 and max_speed.
-        
-        Args:
-            new_speed (float): The new speed of the animal.
-        """
-        self.__curr_speed = max(0, min(new_speed, self.__max_speed))
+        self._velocity += acceleration
 
-    def set_position(self, new_position: tuple) -> None:
-        """
-        Sets the new position of the animal. Does not check if new position is within proper bounds.
+        if self._velocity.length() > self._max_speed:
+            self._velocity = self._velocity.normalize() * self._max_speed
 
-        Args:
-            new_position (tuple): The new (x, y) position of the animal.
-        """
-        if len(new_position) != 2:
-            raise ValueError("Position must be a tuple of length 2.")
-        self.__position = new_position
+        self._position += self._velocity
+
+    # def set_position(self, new_position: tuple) -> None:
+    #     """
+    #     Sets the new position of the animal. Does not check if new position is within proper bounds.
+
+    #     Args:
+    #         new_position (tuple): The new (x, y) position of the animal.
+    #     """
+    #     if len(new_position) != 2:
+    #         raise ValueError("Position must be a tuple of length 2.")
+    #     self.__position = new_position
--- predator.py
+++ predator.py
@@ -1,30 +1,32 @@
 from animal import Animal
 from memory import Memory
 from genome import Genome
+from pygame.math import Vector2
 
 class Predator(Animal):
     def __init__(self, 
-    position: tuple,
-    direction: float, 
+    position: Vector2, 
+    velocity: Vector2,
+    mass: float,
+    max_force: float,
+    max_speed: float,
+    orientation: float,
     vision_range: float, 
     fov: float, 
-    max_speed: float, 
-    curr_speed: float, 
     memory: Memory, 
     genome: Genome, 
     radius: float, 
     color: tuple):
-        """
-        Args:
-            position (tuple): The initial (x, y) position of the animal.
-            direction (float): The initial direction in radians, normalized between 0 and 2π.
-            vision_range (float): The vision range of the animal.
-            fov (float): The field of view of the animal in radians.
-            max_speed (float): The maximum speed the animal.
-            curr_speed (float): The current speed of the animal.
-            memory (Memory): Memory of the animal.
-            genome (Genome): Genetic information about the animal.
-            radius (float): The radius of the animal.
-            color (tuple): The RGB color of the animal.
-        """
-        super().__init__(position, direction, vision_range, fov, max_speed, curr_speed, memory, genome, radius, color)
+        super().__init__(
+        position,
+        velocity,
+        mass,
+        max_force,
+        max_speed,
+        orientation,
+        vision_range,
+        fov,
+        memory,
+        genome,
+        radius,
+        color)
