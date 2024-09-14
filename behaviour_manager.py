d90604015e698349370fa46afb39cd866be99591
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-14 17:15:28 -0500

    Create BehaviourManager class

---
+++ behaviour_manager.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+from animal import Animal
+
+class BehaviourManager(ABC):
+    def __init__(self):
+        self._behaviours = []
+    @abstractmethod
+    def choose_behaviour(animal: Animal):
+        pass
