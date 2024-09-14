e20dcfef84ef4d0c0dda00ec2e3ed9e1094e694d
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-14 17:16:50 -0500

    Use BehaviourManager to decide behaviours

--- sim_controller.py
+++ sim_controller.py
@@ -1,14 +1,19 @@
 from environment import Environment
 from behaviour_results import *
-from search_for_plant_behaviour import SearchForPlantBehaviour
+from behaviours.prey_behaviours import *
 from prey import Prey
+from prey_behaviour_manager import PreyBehaviourManager
+from pygame.math import Vector2
 
 class SimController:
     def __init__(self, environment):
-        self.__environment = environment
+        self._environment = environment
+        self._prey_behaviour_manager = PreyBehaviourManager()
 
     def update_state(self) -> None:
-        prey = self.__environment.get_prey()
+        prey = self._environment.get_prey()
         for p in prey:
-            test = SearchForPlantBehaviour(p, self.__environment.get_plants())
-            behaviour_result = test.execute()
+            chosen_behaviour = self._prey_behaviour_manager.choose_behaviour(p, self._environment)
+            behaviour_result = chosen_behaviour.execute()
+            if behaviour_result:
+                behaviour_result.apply_to_environment(self._environment)
