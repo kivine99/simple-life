2e23c486fa34769debb4007a97e7bdbd5df15c87
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-14 09:55:35 -0500

    Remove name mangling

--- memory.py
+++ memory.py
@@ -2,12 +2,11 @@ from behaviour import Behaviour
 from typing import Optional
 
 class Memory:
-
     def __init__(self):
-        self.__last_behaviour = None
+        self._last_behaviour = None
 
     def get_last_behaviour(self) -> Optional[Behaviour]:
-        return self.__last_behaviour
+        return self._last_behaviour
 
     def set_last_behaviour(self, new_behaviour: Behaviour) -> None:
-        self.__last_behaviour = new_behaviour
+        self._last_behaviour = new_behaviour
