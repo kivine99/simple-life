d2721183c69a62cddfe9155afe54bfb8a0a15fbe
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-17 20:15:49 -0500

    Add class method

--- predator.py
+++ predator.py
@@ -4,6 +4,26 @@ from genome import Genome
 from pygame.math import Vector2
 
 class Predator(Animal):
+
+    _eat_plant_energy_gained = 0
+    _move_energy_lost = 0
+
+    @classmethod
+    def get_move_energy_lost(cls) -> float:
+        return cls._move_energy_lost
+
+    @classmethod
+    def get_eat_energy_gained(cls) -> float:
+        return cls._eat_plant_energy_gained
+
+    @classmethod
+    def set_move_energy_lost(cls, amount_lost: float) -> None:
+        cls._move_energy_lost = amount_lost 
+
+    @classmethod
+    def set_eat_energy_gained(cls, amount_gained: float) -> None:
+        cls._eat_plant_energy_gained = amount_gained
+
     def __init__(self, 
     position: Vector2, 
     velocity: Vector2,
@@ -15,6 +35,7 @@ class Predator(Animal):
     fov: float, 
     memory: Memory, 
     genome: Genome, 
+    energy: float,
     radius: float, 
     color: tuple):
         super().__init__(
@@ -28,5 +49,6 @@ class Predator(Animal):
         fov,
         memory,
         genome,
+        energy,
         radius,
         color)
