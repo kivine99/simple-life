2342c7c54973aa50bf21e1b849d6e633bb58b7fa
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-17 20:12:28 -0500

    Add spacing

--- behaviour_manager.py
+++ behaviour_manager.py
@@ -2,8 +2,10 @@ from abc import ABC, abstractmethod
 from animal import Animal
 
 class BehaviourManager(ABC):
+
     def __init__(self):
         self._behaviours = []
+        
     @abstractmethod
     def choose_behaviour(animal: Animal):
         pass
