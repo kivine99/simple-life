b0b59b369d9f838a425c855e85d0201e439e4e69
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-17 20:13:32 -0500

    Change setup

--- environment_setup.py
+++ environment_setup.py
@@ -9,6 +9,7 @@ from typing import Tuple
 import math
 
 class EnvironmentSetup:
+    
     def __init__(self, map_size: Tuple[int, int], num_prey: int, num_predators: int, num_plants: int):
         self._map_size = map_size
         self._num_prey = num_prey
@@ -26,28 +27,30 @@ class EnvironmentSetup:
                 position=prey_position,
                 velocity=Vector2(0, 0),  
                 mass=1.0,
-                max_force=.1,
+                max_force=.04,
                 max_speed=2.0,
                 orientation=0.0,
                 vision_range=150.0,
                 fov=3*math.pi/4,
                 memory=Memory(), 
                 genome=None,
+                energy=100,
                 radius=5.0,
                 color=(255, 0, 0)  
             )
             prey_list.append(prey)
 
         for i in range(self._num_plants):
-            plant_position = self._random_position()
+            plant_position = [random.uniform(0, self._map_size[0]),
+                              random.uniform(0, self._map_size[1])]
             plant = Plant(
                 position=plant_position,
-                size=5.0,
+                radius=5,
                 color=(0, 255, 0) 
             )
             plant_list.append(plant)
 
-        return Environment(prey_list, predator_list, plant_list)
+        return Environment(self._map_size, prey_list, predator_list, plant_list)
 
     def _random_position(self) -> Vector2:
         return Vector2(
