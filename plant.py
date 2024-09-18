c3e755deef0f2adf978a19a8b0f23027651ec8f2
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-17 20:15:31 -0500

    Change position to a tuple

--- plant.py
+++ plant.py
@@ -1,15 +1,16 @@
 from pygame.math import Vector2
 from typing import Tuple
 class Plant:
-    def __init__(self, position: Vector2, color: Tuple[int, int, int], size: float):
+    
+    def __init__(self, position: Tuple[int, int], color: Tuple[int, int, int], radius: float):
         self._position = position
         self._color = color
-        self._size = size
+        self._radius = radius
 
-    def get_size(self) -> float:
-        return self._size
+    def get_radius(self) -> float:
+        return self._radius
 
-    def get_position(self) -> Vector2:
+    def get_position(self) -> Tuple[int, int]:
         return self._position
 
     def get_color(self) -> Tuple[int, int, int]:
