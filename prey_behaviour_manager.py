6428be6075b6cc4123a7dcce8e9d63c7e7fc767c
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-17 20:16:04 -0500

    Add behaviour

--- prey_behaviour_manager.py
+++ prey_behaviour_manager.py
@@ -1,19 +1,23 @@
 from behaviour_manager import BehaviourManager
 from behaviours.prey_behaviours.wander_behaviour import WanderBehaviour
+from behaviours.prey_behaviours.eat_plant_behaviour import EatPlantBehaviour
 from behaviours.prey_behaviours.move_toward_plant_behaviour import MoveTowardPlantBehaviour
 from behaviours.behaviour import Behaviour
 from prey import Prey
+import math
 from typing import Type
 from environment import Environment
 
 class PreyBehaviourManager(BehaviourManager):
+
     def __init__(self):
         super().__init__()
         self._behaviours.append(WanderBehaviour)
         self._behaviours.append(MoveTowardPlantBehaviour)
+        self._behaviours.append(EatPlantBehaviour)
 
     def choose_behaviour(self, prey: Prey, environment: Environment) -> Behaviour:
-        highest_priority = -float('inf')
+        highest_priority = -math.inf
         chosen_behaviour = None
         for behaviour in self._behaviours:
             behaviour_priority = behaviour.get_priority(prey, environment)
