11d1d0870f345d0da0b518b26452458ec461c103
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-18 10:24:26 -0500

    Change map from tuple to list

--- plant.py
+++ plant.py
@@ -1,8 +1,8 @@
 from pygame.math import Vector2
-from typing import Tuple
+from typing import Tuple, List
 class Plant:
     
-    def __init__(self, position: Tuple[int, int], color: Tuple[int, int, int], radius: float):
+    def __init__(self, position: list[int], color: Tuple[int, int, int], radius: float):
         self._position = position
         self._color = color
         self._radius = radius
@@ -10,7 +10,7 @@ class Plant:
     def get_radius(self) -> float:
         return self._radius
 
-    def get_position(self) -> Tuple[int, int]:
+    def get_position(self) -> List[int]:
         return self._position
 
     def get_color(self) -> Tuple[int, int, int]:
