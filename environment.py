17ecada55cb135b5fd289a9b3e77884427e14c12
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-17 20:14:01 -0500

    Add behaviour results

--- environment.py
+++ environment.py
@@ -2,12 +2,17 @@ from prey import Prey
 from predator import Predator
 from plant import Plant
 from typing import Tuple
+from animal import Animal
+import random
 
 class Environment:
+    
     def __init__(self, 
+    map_dimensions: tuple,
     prey: list[Prey], 
     predators: list[Predator],
     plants: list[Plant]):
+        self._map_dimensions = map_dimensions
         self._prey = prey
         self._predators = predators
         self._plants = plants
@@ -27,5 +32,56 @@ class Environment:
     def add_predator(self, predator_to_add: Predator) -> None:
         self._predator.append(predator_to_add)
 
+    def add_random_plant(self) -> None:
+        plant_position = [random.uniform(0, self._map_dimensions[0]),
+                              random.uniform(0, self._map_dimensions[1])]
+        plant = Plant(
+            position=plant_position,
+            radius=5.0,
+            color=(0, 255, 0) 
+        )
+        self._plants.append(plant)
+
     def remove_plant(self, plant_to_remove: Plant) -> None:
         self._plants.remove(plant_to_remove)
+        self.add_random_plant()
+
+    def update_animal_energy(self, animal: Animal, energy_amount: float) -> None:
+        animal.update_energy(energy_amount)
+
+    def remove_animal(self, animal_to_remove: Animal) -> None:
+        try: #TODO: consider refactoring this
+            if isinstance(animal_to_remove, Prey):
+                self._prey.remove(animal_to_remove)
+            elif isinstance(animal_to_remove, Predator):
+                self._predators.remove(animal_to_remove)
+        except ValueError:
+            pass 
+
+    def move_animal(self, animal_to_move, velocity):
+        position = animal_to_move.get_position()
+
+        last_behaviour = animal_to_move.get_memory().get_last_behaviour()
+
+        if last_behaviour and (position[0] < 0 or position[0] > self._map_dimensions[0] or
+                            position[1] < 0 or position[1] > self._map_dimensions[1]):
+            last_behaviour.invert_previous_force_angle()
+
+        if position[0] < 0:
+            position[0] = 0 
+            velocity.x = -velocity.x 
+
+        elif position[0] > self._map_dimensions[0]:
+            position[0] = self._map_dimensions[0]
+            velocity.x = -velocity.x
+
+        if position[1] < 0:
+            position[1] = 0
+            velocity.y = -velocity.y 
+
+        elif position[1] > self._map_dimensions[1]:
+            position[1] = self._map_dimensions[1]
+            velocity.y = -velocity.y
+
+        animal_to_move.set_velocity(velocity)
+        animal_to_move.update_position()
