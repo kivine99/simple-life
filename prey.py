ef4bbf533f3b1f3f012c83515a6753d87c12af10
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-17 20:16:23 -0500

    Add nearest_plant method

--- prey.py
+++ prey.py
@@ -2,8 +2,30 @@ from animal import Animal
 from memory import Memory
 from genome import Genome
 from pygame.math import Vector2
+from plant import Plant
+import math
 
 class Prey(Animal):
+
+    _eat_energy_gained = 40
+    _move_energy_lost = -.01
+
+    # @classmethod
+    # def get_move_energy_lost(cls) -> float:
+    #     return cls._move_energy_lost
+
+    # @classmethod
+    # def get_eat_energy_gained(cls) -> float:
+    #     return cls._eat_plant_energy_gained
+
+    # @classmethod
+    # def set_move_energy_lost(cls, amount_lost: float) -> None:
+    #     cls._move_energy_lost = amount_lost 
+
+    # @classmethod
+    # def set_eat_energy_gained(cls, amount_gained: float) -> None:
+    #     cls._eat_plant_energy_gained = amount_gained
+
     def __init__(self, 
     position: Vector2, 
     velocity: Vector2,
@@ -15,6 +37,7 @@ class Prey(Animal):
     fov: float, 
     memory: Memory, 
     genome: Genome, 
+    energy: float,
     radius: float, 
     color: tuple):
         super().__init__(
@@ -28,5 +51,19 @@ class Prey(Animal):
         fov,
         memory,
         genome,
+        energy,
         radius,
         color)
+
+    def nearest_plant(self, plants: list[Plant]) -> Plant:
+        nearest_plant = None
+        min_distance = math.inf
+
+        for plant in plants:
+            plant_position = plant.get_position()
+            distance = self._position.distance_to(plant_position)
+            if distance < min_distance:
+                min_distance = distance
+                nearest_plant = plant
+
+        return nearest_plant
