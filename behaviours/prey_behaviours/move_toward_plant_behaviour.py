0c5ca2e42c7af5f4f86a8dfb2b59142f50e8a6cb
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-23 14:14:01 -0500

    Use new Prey methods

--- behaviours/prey_behaviours/move_toward_plant_behaviour.py
+++ behaviours/prey_behaviours/move_toward_plant_behaviour.py
@@ -1,6 +1,6 @@
 from behaviours.behaviour import Behaviour
-from prey import Prey
-from plant import Plant
+from entities.prey import Prey
+from entities.plant import Plant
 from typing import Tuple
 import math
 from pygame.math import Vector2
@@ -12,7 +12,7 @@ class MoveTowardPlantBehaviour(Behaviour):
     @staticmethod
     def get_priority(prey: Prey, environment: Environment):
         for plant in environment.get_plants():
-            if prey.is_within_view(plant.get_position()):
+            if prey.get_vision().is_within_view(prey.get_movement().get_position(), plant.get_position()):
                 return 200
         return 20
 
@@ -23,21 +23,26 @@ class MoveTowardPlantBehaviour(Behaviour):
     from pygame.math import Vector2
 
     def execute(self) -> MoveBehaviourResult:
-        energy_cost = 0 
-
         nearest_plant = self._prey.nearest_plant(self._environment.get_plants())
+
+        #Do nothing if there is no plant 
+        if nearest_plant is None:
+            return
+
         target_position = nearest_plant.get_position()
-        prey_position = self._prey.get_position()
+
+        prey_position = self._prey.get_movement().get_position()
+        prey_max_speed = self._prey.get_movement().get_max_speed()
+        prey_velocity = self._prey.get_movement().get_velocity()
+        prey_energy_lost = self._prey.get_energy().get_move_energy_lost()
         
-        desired_direction = Vector2(target_position[0] - prey_position[0], target_position[1] - prey_position[1])
+        desired_direction = Vector2(target_position.x - prey_position.x, target_position.y - prey_position.y)
 
         if desired_direction.length() != 0:
             desired_direction = desired_direction.normalize()
         
-        desired_velocity = desired_direction * self._prey.get_max_speed()
+        desired_velocity = desired_direction * prey_max_speed
 
-        steering_force = desired_velocity - self._prey.get_velocity()
+        steering_force = desired_velocity - prey_velocity
         
-        new_velocity = self._prey.calculate_new_velocity(steering_force)
-
-        return MoveBehaviourResult(self._prey, new_velocity, self._prey.get_move_energy_lost())
+        return MoveBehaviourResult(self._prey, self, steering_force, prey_energy_lost)
