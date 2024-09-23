7503f27d0d4e21d3f0c1f9d71d1422033ef195c3
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-23 14:10:40 -0500

    Add previous steering force to memory

--- memory.py
+++ memory.py
@@ -1,13 +1,29 @@
 from behaviours.behaviour import Behaviour
 from typing import Optional
+from pygame.math import Vector2
 
 class Memory:
     
     def __init__(self):
         self._last_behaviour = None
+        self._previous_steering_force = None
 
     def get_last_behaviour(self) -> Optional[Behaviour]:
         return self._last_behaviour
 
     def set_last_behaviour(self, new_behaviour: Behaviour) -> None:
         self._last_behaviour = new_behaviour
+
+    def get_previous_steering_force(self) -> Vector2:
+        return self._previous_steering_force
+
+    def set_previous_steering_force(self, new_force: Vector2) -> None:
+        self._previous_steering_force = new_force
+
+    def inverse_previous_steering_force_x(self) -> None:
+        if self._previous_steering_force:
+            self._previous_steering_force.x *= -1
+
+    def inverse_previous_steering_force_y(self) -> None:
+        if self._previous_steering_force:
+            self._previous_steering_force.y *= -1
