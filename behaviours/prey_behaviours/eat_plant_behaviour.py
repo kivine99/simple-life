680a7f6e37b188b35c42fa36cfad8e24955a6b26
Kevin Breckman <kevinbreckman@gmail.com>
2024-09-23 14:12:58 -0500

    Fix prey method calls

--- behaviours/prey_behaviours/eat_plant_behaviour.py
+++ behaviours/prey_behaviours/eat_plant_behaviour.py
@@ -1,5 +1,6 @@
 from behaviours.behaviour import Behaviour
-from prey import Prey
+from entities.prey import Prey
+from entities.plant import Plant
 from environment import Environment
 from behaviour_results.eat_plant_behaviour_result import EatPlantBehaviourResult
 import math
@@ -10,7 +11,7 @@ class EatPlantBehaviour(Behaviour):
     def get_priority(prey: Prey, environment: Environment):
         #TODO: optimize this
         nearest_plant = prey.nearest_plant(environment.get_plants())
-        distance_to_plant = math.dist(prey.get_position(), nearest_plant.get_position())#prey.get_position().distance_to(nearest_plant.get_position())
+        distance_to_plant = math.dist(prey.get_movement().get_position(), nearest_plant.get_position())#prey.get_position().distance_to(nearest_plant.get_position())
         if distance_to_plant < 3:
             return 100000 #TODO: maybe it shouldn't always be eaten?
         return 0
@@ -21,6 +22,6 @@ class EatPlantBehaviour(Behaviour):
 
     def execute(self) -> EatPlantBehaviourResult:
         nearest_plant = self._prey.nearest_plant(self._environment.get_plants())
-        distance_to_plant = math.dist(self._prey.get_position(), nearest_plant.get_position())#self._prey.get_position().distance_to(nearest_plant.get_position())
+        # distance_to_plant = math.dist(self._prey.get_movement().get_position(), nearest_plant.get_position())
 
-        return EatPlantBehaviourResult(self._prey, nearest_plant, self._prey.get_eat_energy_gained()) #TODO: should I check if it's close enough?
+        return EatPlantBehaviourResult(self._prey, nearest_plant, self._prey.get_energy().get_eat_energy_gained()) #TODO: should I check if it's close enough?
